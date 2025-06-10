fe2efe04b5adf4dd43c57093c3d77084e8c863c2
Catalina Reyes <nreyessuarez@ogs.it>
2025-06-10 15:28:04 +0200

    test to add orcid icon

--- src/settings.ts
+++ src/settings.ts
@@ -12,18 +12,20 @@ export const profile = {
 export const social = {
 	email: 'nreyessuarez@ogs.it',
 	linkedin: 'https://www.linkedin.com/in/catalina-reyes-su%C3%A1rez-56b235242/',
-	x: 'https://www.x.com/',
+	x: 'https://x.com/GeOkTa/',
 	github: 'https://github.com/Geeokta',
-	orcid: 'https://orcid.org/0000-0002-3906-471X',
+	gitlab: '',
 	scholar: 'https://scholar.google.com/citations?user=i6wrmqgAAAAJ&hl=en',
-	//website: 'https://Geeokta.github.io',
+	orcid: 'https://orcid.org/0000-0002-3906-471X',
+	inspire: '',
+	arxiv: '',
 }
 
 export const template = {
 	website_url: 'https://Geeokta.github.io', // Astro needs to know your site’s deployed URL to generate a sitemap. It must start with http:// or https://
 	menu_left: false,
 	transitions: true,
-	lightTheme: 'lofi', // Select one of the Daisy UI Themes or create your own
+	lightTheme: 'bumblebee', // Select one of the Daisy UI Themes or create your own
 	darkTheme: 'synthwave', // Select one of the Daisy UI Themes or create your own
 	excerptLength: 200,
 	postPerPage: 5,
